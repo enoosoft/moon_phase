e6c12036664a1f29af6077ae092accb80d3d2ab9
EnooNote <as.enoosoft@gmaill.com>
2021-10-08 12:47:34 +0900

    enable web

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,6 @@
+## 1.0.0+2
+* Enable platform web
+## 1.0.0+1
+* Update README.md
 ## 1.0.0
 * Initial release.
---
+++ lib/moon_phase_web.dart
@@ -0,0 +1,47 @@
+import 'dart:async';
+// In order to *not* need this ignore, consider extracting the "web" version
+// of your plugin as a separate package, instead of inlining it in the same
+// package as the core of your plugin.
+// ignore: avoid_web_libraries_in_flutter
+import 'dart:html' as html show window;
+
+import 'package:flutter/services.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+export 'moon_painter.dart' show MoonPainter;
+export 'moon_widget.dart' show MoonWidget;
+export 'moon_phase.dart' show MoonPhase;
+
+/// A web implementation of the MoonPhase plugin.
+class MoonPhaseWeb {
+  static void registerWith(Registrar registrar) {
+    final MethodChannel channel = MethodChannel(
+      'moon_phase',
+      const StandardMethodCodec(),
+      registrar,
+    );
+
+    final pluginInstance = MoonPhaseWeb();
+    channel.setMethodCallHandler(pluginInstance.handleMethodCall);
+  }
+
+  /// Handles method calls over the MethodChannel of this plugin.
+  /// Note: Check the "federated" architecture for a new way of doing this:
+  /// https://flutter.dev/go/federated-plugins
+  Future<dynamic> handleMethodCall(MethodCall call) async {
+    switch (call.method) {
+      case 'getPlatformVersion':
+        return getPlatformVersion();
+      default:
+        throw PlatformException(
+          code: 'Unimplemented',
+          details: 'moon_phase for web doesn\'t implement \'${call.method}\'',
+        );
+    }
+  }
+
+  /// Returns a [String] containing the version of the platform.
+  Future<String> getPlatformVersion() {
+    final version = html.window.navigator.userAgent;
+    return Future.value(version);
+  }
+}
